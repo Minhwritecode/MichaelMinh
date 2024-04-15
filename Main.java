4c730ca1a012728a1bde8b99b965b5c128172376
Minhwritecode <146536891+Minhwritecode@users.noreply.github.com>
2024-04-15 23:32:48 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import Controllers.texteditorController;
+import Views.texteditorView;
+
+public class Main {
+    public static void main(String[] args) {
+        new texteditorController(new texteditorView());
+    }
+}
